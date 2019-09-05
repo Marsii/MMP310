0b9687ed31753f32a601c6cfd0f4f998773ed41c
N <naomijcmarshall@gmail.com>
2019-09-05 18:46:16 -0400

    working story

--- story/sketch.js
+++ story/sketch.js
@@ -1,6 +1,18 @@
+/*
+    story sketch
+    9-5-19
+    Marshall
+    mmp 310
+*/
+
+//character variables
 var x =250;
 var y =200;
-var s =300;
+var faceSize =200;
+var eyeSize =50;
+var skincolor= 'pink';
+var eyeOffset= 25;
+
 
 var greeting= '"Hello!"';
 function setup() {
@@ -9,7 +21,14 @@ function setup() {
 function draw() {    
     background('black');
     textSize(40);
-    text(greeting,x,y);
+    //text(greeting,x,y);
+    
+    
+    fill(skincolor);
+    stroke('white')
+    strokeWeight(3);
     ellipse(x,y,s);
+    rect(x-50,y-50,eyesize,eyesize);//left eye
+    rect(x+50,y+50,eyesize,eyesize);//left eye
 
 }
