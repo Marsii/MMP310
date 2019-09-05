267eef023e93b6c6a16afc9e586e1c3406904264
N <naomijcmarshall@gmail.com>
2019-09-05 19:13:52 -0400

    story start development

--- story/sketch.js
+++ story/sketch.js
@@ -6,29 +6,30 @@
 */
 
 //character variables
-var x =250;
-var y =200;
-var faceSize =200;
-var eyeSize =50;
-var skincolor= 'pink';
-var eyeOffset= 25;
+var x = 250;
+var y = 200;
+var faceSize = 200;
+var eyeSize = 50;
+var c = 'green';
+var eyeOffset = 25;
+var greeting = '"Hello Earthling!"';
 
-
-var greeting= '"Hello!"';
 function setup() {
     createCanvas(windowWidth,600);
-}
-function draw() {    
-    background('black');
-    textSize(40);
-    //text(greeting,x,y);
+    background('black'); 
     
+    textSize(30);
+    fill('white');
+    text(greeting,x+100,y-50);
     
-    fill(skincolor);
-    stroke('white')
-    strokeWeight(3);
-    ellipse(x,y,s);
-    rect(x-50,y-50,eyesize,eyesize);//left eye
-    rect(x+50,y+50,eyesize,eyesize);//left eye
-
+   
+    fill(c);
+    stroke('white');
+    ellipse(x, y, faceSize);//face
+    fill('black');
+    rotate(1*PI/37);
+    ellipse(x - eyeOffset, y - eyeOffset, eyeSize, 70);//left eye
+    
+    ellipse(x + eyeOffset + 25, y - eyeOffset, eyeSize, 70);//right eye
+    
 }
