3e6dd25a421d0c1ee6be9fcf4780858bcf846cc9
N <naomijcmarshall@gmail.com>
2019-09-05 18:29:57 -0400

    js experimenting

--- story/sketch.js
+++ story/sketch.js
@@ -1,6 +1,15 @@
+var x =250;
+var y =200;
+var s =300;
+
+var greeting= '"Hello!"';
 function setup() {
     createCanvas(windowWidth,600);
-    
+}
+function draw() {    
     background('black');
-    
+    textSize(40);
+    text(greeting,x,y);
+    ellipse(x,y,s);
+
 }
