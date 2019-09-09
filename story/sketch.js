9a70c574f124d21473f109c7baf655b5ed3a5fa8
Marsii <42751468+Marsii@users.noreply.github.com>
2019-09-09 01:32:35 -0400

    second character creation

--- story/sketch.js
+++ story/sketch.js
@@ -12,13 +12,14 @@ var faceSize = 150;
 var eyeSize = 30;
 var c = 'green' ;
 var eyeOffset = 25;
-var greeting = '"Hello Earthling!"';
+var greeting = '"Greetings!"';
+var h = 400;
 
 function setup() {
     createCanvas(windowWidth,600);
     background('black'); 
     
-    textSize(30);
+    textSize(15);
     fill('white');
     text(greeting,x+100,y-50);
     
@@ -30,14 +31,35 @@ function setup() {
     fill('black');
     stroke('white');
     //rotate();
-    ellipse(x - eyeOffset, y  , eyeSize, 70);//left eye
+    ellipse(x - eyeOffset, y  , eyeSize, 60);//left eye
     
-    ellipse(x + eyeOffset + 25, y, eyeSize, 70);//right eye
+    ellipse(x + eyeOffset + 25, y, eyeSize, 60);//right eye
     noStroke();
     fill(50, 186, 78);
     triangle(210, 375, 258, 220, 296, 375);//body
     rect(240, 80, 15, 55);//antenna
     
-    rect(230, 360, 15, 75);//leg
+    rect(230, 360, 15, 65);//left leg
+    rect(270, 360, 15, 65);//right leg
     
+    
+    //Blue Character
+    fill(50, 186, 178);
+    
+    ellipse(h + 200, y, faceSize);//face
+    fill('black');
+    stroke('white');
+    //rotate();
+    ellipse(h - eyeOffset + 175, y  , eyeSize, 60);//left eye
+    ellipse(h + 187.5, y - 22 , eyeSize, 40);//middle eye
+    ellipse(h + eyeOffset + 200, y, eyeSize, 60);//right eye
+    noStroke();
+    fill(50, 186, 178);
+    triangle(560, 375, 608, 220, 646, 375);//body
+    rect(580, 80, 15, 65);//antenna
+    rect(600, 80, 15, 65);//antenna
+    ellipse(597,80, 40);//antenna top
+    
+    rect(580, 360, 15, 65);//left leg
+    rect(620, 360, 15, 65);//right leg
 }
