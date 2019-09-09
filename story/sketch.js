204f788b69413dab31fe934d65b102038b43a4e4
Marsii <42751468+Marsii@users.noreply.github.com>
2019-09-09 01:12:45 -0400

    working on story, first character

--- story/sketch.js
+++ story/sketch.js
@@ -8,9 +8,9 @@
 //character variables
 var x = 250;
 var y = 200;
-var faceSize = 200;
-var eyeSize = 50;
-var c = 'green';
+var faceSize = 150;
+var eyeSize = 30;
+var c = 'green' ;
 var eyeOffset = 25;
 var greeting = '"Hello Earthling!"';
 
@@ -23,13 +23,21 @@ function setup() {
     text(greeting,x+100,y-50);
     
    
-    fill(c);
-    stroke('white');
+//Green Character
+    fill(50, 186, 78);
+    
     ellipse(x, y, faceSize);//face
     fill('black');
-    rotate(1*PI/37);
-    ellipse(x - eyeOffset, y - eyeOffset, eyeSize, 70);//left eye
+    stroke('white');
+    //rotate();
+    ellipse(x - eyeOffset, y  , eyeSize, 70);//left eye
     
-    ellipse(x + eyeOffset + 25, y - eyeOffset, eyeSize, 70);//right eye
+    ellipse(x + eyeOffset + 25, y, eyeSize, 70);//right eye
+    noStroke();
+    fill(50, 186, 78);
+    triangle(210, 375, 258, 220, 296, 375);//body
+    rect(240, 80, 15, 55);//antenna
+    
+    rect(230, 360, 15, 75);//leg
     
 }
