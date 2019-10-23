46318571241b85647ffc7f20a602e4d884a282de
Marsii <naomijcmarshall@gmail.com>
2019-10-23 19:56:55 -0400

    just making sure

--- array/sketch.js
+++ array/sketch.js
@@ -33,6 +33,27 @@ function gren(y) {
 }
 
 /*
+function gren(x, y, size) {
+    fill('plum')
+
+    ellipse(x, y, size); //face
+    fill('black');
+    stroke('white');
+    //rotate();
+    ellipse(x - size/5, y, size/5, size/2.5); //left eye
+    ellipse(x + size/5, y, size/5, size/2.5); //right eye
+    noStroke();
+    fill('plum');
+    triangle(x-50, y*1.875, x, y, x+50, y*1.875); //body
+    rect(x - 8, y/2.5, size/10, size/3); //antenna
+    ellipse(x, y/2.5, size/3); //antenna top
+
+    rect(x/1.15, y*1.8, size/10, size/3); //left leg
+    rect(x/.95, y*1.8, size/10, size/3); //right leg
+}
+*/
+
+/*
 function gren(x, y, size, xbody, ybody) {
     fill(50, 186, 78);
 
