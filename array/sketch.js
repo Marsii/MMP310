e5fd5ff29fd8c219e99b604bdabd3e9c088feecc
Marsii <naomijcmarshall@gmail.com>
2019-10-28 18:12:04 -0400

    fixed main issue

--- array/sketch.js
+++ array/sketch.js
@@ -1,19 +1,19 @@
 function setup() {
     createCanvas(800, 800);
-    background('black');
+    //background('black');
     //alien(600, 200, 150, 0, 0);
-    gren(200);
+    gren(200,250,150);
     //semi-working but flawed
     //gren(250, 200, 150);
 
 }
-
-function gren(y) {
+/*
+function gren(x) {
 //function gren(x, y, size)
 //need to make body change properly based on size
     fill(50, 186, 78);
     
-    x = y*1.25
+    y = x/1.25
     size = y*.75
 
     ellipse(x, y, size); //face
@@ -24,18 +24,18 @@ function gren(y) {
     ellipse(x + size/5, y, size/5, size/2.5); //right eye
     noStroke();
     fill(50, 186, 78);
-    triangle(x-50, y*1.875, x, y, x+50, y*1.875); //body
+    triangle(x-x/5, y*1.875, x, y, x+x/5, y*1.875); //body
     rect(x - 8, y/2.5, size/10, size/3); //antenna
     ellipse(x, y/2.5, size/3); //antenna top
 
     rect(x/1.15, y*1.8, size/10, size/3); //left leg
     rect(x/.95, y*1.8, size/10, size/3); //right leg
 }
+*/
 
-/*
 function gren(x, y, size) {
     fill('plum')
-
+    noStroke();
     ellipse(x, y, size); //face
     fill('black');
     stroke('white');
@@ -44,14 +44,18 @@ function gren(x, y, size) {
     ellipse(x + size/5, y, size/5, size/2.5); //right eye
     noStroke();
     fill('plum');
-    triangle(x-50, y*1.875, x, y, x+50, y*1.875); //body
-    rect(x - 8, y/2.5, size/10, size/3); //antenna
-    ellipse(x, y/2.5, size/3); //antenna top
+    triangle(x-x/5, y*1.875, x, y, x+x/5, y*1.875); //body
+    strokeWeight(size/15);
+    stroke('plum');
+    line(x, y/2, x, y);
+    //rect(x - 8, y/2.5, size/10, size/3); //antenna
+    noStroke();
+    ellipse(x, y/2, size/3); //antenna top
 
     rect(x/1.15, y*1.8, size/10, size/3); //left leg
     rect(x/.95, y*1.8, size/10, size/3); //right leg
 }
-*/
+
 
 /*
 function gren(x, y, size, xbody, ybody) {
